b0c14bc3aa3fce6376b9ec36897a85da584a338a
BewareMyPower <xyzinfernity@163.com>
2018-03-02 19:59:08 +0800

    更新算法

--- 栈的压入弹出序列.cc
+++ 栈的压入弹出序列.cc
@@ -4,17 +4,30 @@
  * 序列4，5,3,2,1是该压栈序列对应的一个弹出序列，
  * 但4,3,5,1,2就不可能是该压栈序列的弹出序列。（注意：这两个序列的长度是相等的）
  */
-
-// 思路: 对pushV={1,2,3,4,5}和popV={4,5,3,2,1}的输入而言
-// 第一步: 先找到4在pushV的下标(3)，因此4在弹出时，1,2,3均已入栈
-// 因此1,2,3在popV中的先后顺序必须是3->2->1
-// 第二步: 到5在pushV的下标(4)，注意，由于4比5先出栈，所以5出栈时4不在栈中
-// 所以第一步中必须设置4已经被访问过来排除。
-// 同理，刚才1,2,3的相对顺序已经确认过，所以也无需重新确认。
-// 重复第二步直到遍历完popV即可。
  
 class Solution {
 public:
+    // 思路: 建立辅助栈stk模拟真实的出栈情况
+    bool IsPopOrder(vector<int> pushV,vector<int> popV) {
+        // 假设序列不包含重复元素!!!
+        int n = pushV.size();
+        if (n == 0 || n != (int)popV.size())
+            return false;
+        
+        vector<int> stk;
+        for (int i = 0, j = 0; i < n; ) {  // popV[j]为当前弹出元素
+            stk.push_back(pushV[i++]);
+            // 按照popV的顺序依次出栈
+            while (j < n && !stk.empty() && stk.back() == popV[j]) {
+                j++;
+                stk.pop_back();
+            }
+        }
+        
+        return stk.empty();
+    }
+
+	/*
     int findVal(int x, const vector<int>& v, int n) {
         for (int i = 0; i < n; ++i) {
             if (v[i] == x)
@@ -23,6 +36,13 @@ public:
         return -1;
     }
     
+	// 思路: 对pushV={1,2,3,4,5}和popV={4,5,3,2,1}的输入而言
+	// 第一步: 先找到4在pushV的下标(3)，因此4在弹出时，1,2,3均已入栈
+	// 因此1,2,3在popV中的先后顺序必须是3->2->1
+	// 第二步: 到5在pushV的下标(4)，注意，由于4比5先出栈，所以5出栈时4不在栈中
+	// 所以第一步中必须设置4已经被访问过来排除。
+	// 同理，刚才1,2,3的相对顺序已经确认过，所以也无需重新确认。
+	// 重复第二步直到遍历完popV即可。
     bool IsPopOrder(vector<int> pushV,vector<int> popV) {
         // 假设序列不包含重复元素!!!
         int n = pushV.size();
@@ -55,4 +75,5 @@ public:
         
         return true;
     }
+	*/
 };
