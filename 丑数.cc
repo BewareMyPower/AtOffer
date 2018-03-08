079aee11e6bec94fa03d89f6de64769e5fb816ab
BewareMyPower <xyzinfernity@163.com>
2018-03-08 22:53:00 +0800

    练习

--- 丑数.cc
+++ 丑数.cc
@@ -3,14 +3,49 @@
  * 例如6、8都是丑数，但14不是，因为它包含因子7。
  * 习惯上我们把1当做是第一个丑数。求按从小到大的顺序的第N个丑数。
  */
+
 class Solution {
 public:
+	struct Record {
+        vector<int>& _v;
+        int _mult;
+        int id = 0;
+        Record(vector<int>& v, int mult) : _v(v), _mult(mult) {}
+        
+        int next() const { return _v[id] * _mult; }
+        
+        bool operator < (const Record& rhs) const {
+            return next() < rhs.next();
+        }
+    };
     // 设第k个丑数为a[n]，记录3个位置i,j,k，分别有
     // a[n]>a[i]*2  a[n]>a[j]*3 a[n]>a[k]*5以及
     // a[n]<a[i+1]*2 a[n]<a[j+1]*3 a[n]<a[k+1]*5
     // 那么每次只需要在a[i+1]*2 a[j+1]*3 a[k+1]*5中找到
     // 一个最小的数作为a[n+1]，然后更新相应的i,j,k即可
     int GetUglyNumber_Solution(int index) {
+        if (index <= 0)
+            return 0;
+        vector<int> v = { 1 };
+        v.reserve(index);
+        
+        vector<Record> records;
+        records.emplace_back(v, 2);
+        records.emplace_back(v, 3);
+        records.emplace_back(v, 5);
+        
+        for (int i = 1; i < index; ++i) {
+            auto it = std::min_element(records.begin(), records.end());
+            v.push_back(it->next());
+            // 更新下标
+            for (auto& rec : records)
+                while (rec.next() <= v.back())
+                    rec.id++;
+        }
+        return v.back();
+    }
+	
+/*    int GetUglyNumber_Solution(int index) {
         if (index <= 0)
             return 0;
         vector<int> v = { 1 };
@@ -31,5 +66,5 @@ public:
             }
         }
         return v.back();
-    }
+    }*/
 };
