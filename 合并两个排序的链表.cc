399296763a7411907dab8e7fe83478135b7ef63f
BewareMyPower <xyzinfernity@163.com>
2018-04-10 14:55:40 -0700

    增加递归解法

--- 合并两个排序的链表.cc
+++ 合并两个排序的链表.cc
@@ -11,6 +11,19 @@ struct ListNode {
 };*/
 class Solution {
 public:
+    ListNode* Merge(ListNode* pHead1, ListNode* pHead2)
+    {
+        if (!pHead1)
+            return pHead2;
+        if (!pHead2)
+            return pHead1;
+
+        if (pHead1->val > pHead2->val)
+            swap(pHead1, pHead2);
+        auto newHead1 = pHead1->next;
+        pHead1->next = Merge(newHead1, pHead2);
+        return pHead1;
+    }
 	/*
     ListNode* Merge(ListNode* pHead1, ListNode* pHead2)
     {
