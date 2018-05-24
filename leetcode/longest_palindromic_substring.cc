1f628175130b5d23e0590b7e5457d0e7580d829e
BewareMyPower <xyzinfernity@163.com>
2018-05-25 04:15:35 +0800

    使用回文串构造法

--- leetcode/longest_palindromic_substring.cc
+++ leetcode/longest_palindromic_substring.cc
@@ -1,32 +1,28 @@
 class Solution {
 public:
-    // 先求s和reverse(s)的最长公共子串，若两个子串所处的位置刚好匹配则为最长回文串
+    // 定义中间若干个相同字符看成回文串的中心，比如"abcxxxcba"的中心是xxx
     string longestPalindrome(string s) {
-        int n = s.size();
-        if (n == 0) return "";
-        string rs = s;
-        reverse(rs.begin(), rs.end());
-        vector<vector<int>> dp(n, vector<int>(n));  // dp[i][j]分别为s1[0..i]和s2[0..j]的最长公共后缀长度
-        for (int i = 0; i < n; ++i) {
-            dp[0][i] = (s[0] == rs[i]) ? 1 : 0;
-            dp[i][0] = (s[i] == rs[0]) ? 1 : 0;
-        }
-        for (int i = 1; i < n; ++i) {
-            for (int j = 1; j < n; ++j) {
-                dp[i][j] = (s[i] == rs[j]) ? (dp[i - 1][j - 1] + 1) : 0;
+        int len = s.size();
+        if (len < 2) return s;
+        int max_left = 0, max_right = 0, maxlen = 0;
+        // 判断start + maxlen / 2 < len的原因在于，先通过start求得中心，然后分别向左和向右扩展相同多的字符
+        // 所以start左边和右边至少有maxlen/2个字符，才可能最后构造的回文子串长度大于maxlen
+        for (int start = 0; start < len && start + maxlen / 2 < len; ++start) {
+            int left = start;
+            int right = start;
+            while (right + 1 < len && s[right] == s[right + 1])
+                ++right;  // 此时s[left..right]为相同字符组成的子串
+            // 分别向左和向右扩展
+            while (left > 0 && right + 1 < len && s[left - 1] == s[right + 1]) {
+                --left;
+                ++right;
+            }
+            if (maxlen < right - left + 1) {
+                maxlen = right - left + 1;
+                max_left = left;
+                max_right = right;
             }
         }
-        int xi = 0, xj = 0, maxlen = 0;  // dp[xi][xj]为最大值maxlen
-        // rs[j-maxlen+1]对应s[(n-1)-(j-maxlen+1)]，即i + j - maxlen + 1 == n - 1时两子串位置对应
-        for (int i = 0; i < n; ++i) {
-            for (int j = 0; j < n; ++j) {
-                if (maxlen < dp[i][j] && (i + j - dp[i][j] + 2 == n)) {
-                    maxlen = dp[i][j];
-                    xi = i;
-                    xj = j;
-                }
-            }
-        }
-        return s.substr(xi + 1 - maxlen, maxlen);  // 注意计算子串左边界时要加1
+        return s.substr(max_left, maxlen);
     }
 };
