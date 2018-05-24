7c264dae520478f93e1938aa5874359f76626115
BewareMyPower <xyzinfernity@163.com>
2018-05-25 02:15:06 +0800

    001

---
+++ leetcode/two sum.cc
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> um;  // 记录已遍历过的元素的值与其在nums中的下标
+        for (int i = 0; i < nums.size(); ++i) {
+            int other = target - nums[i];
+            if (um.find(other) != um.end())
+                return {um[other], i};
+            else
+                um[nums[i]] = i;
+        }
+        return {};
+    }
+};
