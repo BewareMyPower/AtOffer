f450c02b9bc05365c87e520ef0821dd82b1e7fee
BewareMyPower <xyzinfernity@163.com>
2018-07-12 11:16:24 +0800

    改用下标取代迭代器

--- leetcode/longest_common_prefix.cc
+++ leetcode/longest_common_prefix.cc
@@ -1,29 +1,25 @@
 #include "common.h"
 
-// 9ms 51.87
+// 4ms 99.85
 class Solution {
 public:
-    string longestCommonPrefix(vector<string>& strs) {
-        int n = strs.size();
-        if (n == 0) return "";
-        if (n == 1) return strs[0];
-        
-        vector<decltype(strs[0].cbegin())> iters(n);
-        for (int i = 0; i < n; ++i)
-            iters[i] = strs[i].cbegin();
-        
-        string prefix = "";
-        while (true) {
-            if (iters[0] == strs[0].cend())
-                return prefix;
-            for (int i = 1; i < n; ++i) {
-                if (iters[i] == strs[i].cend() || *(iters[i]) != *(iters[i - 1]))
-                    return prefix;
-                ++iters[i - 1];
-            }
-            prefix += *iters.back();
-            ++iters.back();
+    bool isEqualAtChar(size_t pos, vector<string>& strs) {
+        if (strs.size() == 0 || pos >= strs[0].size())
+            return false;
+        for (size_t i = 0; i + 1 < strs.size(); ++i) {
+            if (pos >= strs[i + 1].size() || strs[i][pos] != strs[i + 1][pos])
+                return false;
         }
-        return prefix;
+        return true;
+    }
+
+    string longestCommonPrefix(vector<string>& strs) {
+        if (strs.size() > 0) {
+            size_t pos = 0;
+            while (isEqualAtChar(pos, strs))
+                ++pos;
+            return strs[0].substr(0, pos);
+        }
+        return "";
     }
 };
