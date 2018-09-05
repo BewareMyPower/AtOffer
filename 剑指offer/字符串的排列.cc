1522a7ee02b0be40b88d46752f3d3b1270471ef0
BewareMyPower <xyzinfernity@163.com>
2018-09-05 22:41:43 +0800

    使用next_permutation算法支持重复字符的全排列

--- 剑指offer/字符串的排列.cc
+++ 剑指offer/字符串的排列.cc
@@ -1,31 +1,34 @@
 class Solution {
 public:
-    // 注意，这里不能采用传递string&然后swap后复位的方法，因为交换破坏了顺序
-    // 比如"abc"，a和c交换后变成"cba"，然后递归"ba"，"ba"未排序
-    // 如果每次递归对剩下的字符重新排序，那么后面的顺序变了，无法复位
-    void permutation_imp(vector<string>& v, string s, int k) {
-        int n = s.size();  // 这里必须n>0
-        if (k + 1 == n) {
-            v.push_back(s);
-            return;
+    // 手动实现STL的next_permutation
+    bool NextPermutation(string& s) {
+            size_t n = s.size();
+        if (n < 2)
+            return false;
+        size_t i = n - 1;
+        while (i > 0 && s[i - 1] >= s[i])
+            i--;
+        // a[i..n)为右边最大递减序列
+        if (i == 0) {
+                reverse(s.begin(), s.end());
+            return false;
         }
-        unordered_set<char> us;  // 已经交换过的字符
-        for (int i = k; i < n; ++i) {
-            if (us.count(s[i]) == 0) {
-                us.emplace(s[i]);  // 注意是s[i]而非s[k]
-                std::swap(s[k], s[i]);
-                permutation_imp(v, s, k + 1);
-            }
-        }
+        size_t j = n - 1;
+        while (j >= i && s[j] <= s[i - 1])
+            j--;
+        swap(s[i - 1], s[j]);
+        reverse(s.begin() + i, s.end());
+        return true;
     }
-    
+
     vector<string> Permutation(string str) {
+        if (str.empty())
+            return {};
         sort(str.begin(), str.end());
-        vector<string> res;
-        if (str.size() == 0)
-            return res;
-        
-        permutation_imp(res, str, 0);  // str.size()>0
-        return res;
+        vector<string> v;
+        do {
+            v.push_back(str);
+        } while (NextPermutation(str));
+        return v;
     }
 };
