b55c76be495e6ff344f59607653b54b0c9f0db02
BewareMyPower <xyzinfernity@163.com>
2018-09-06 09:51:02 +0800

    更正原来的错误做法

--- 剑指offer/最小的K个数.cc
+++ 剑指offer/最小的K个数.cc
@@ -1,51 +1,36 @@
+#include <queue>
+#include <vector>
+
+using std::priority_queue;
+using std::vector;
+
 /**
  * 输入n个整数，找出其中最小的K个数。
  * 例如输入4,5,1,6,2,7,3,8这8个数字，则最小的4个数字是1,2,3,4,。
  */
 class Solution {
 public:
-	// 直接用优先队列来做，注意默认std::less是最大堆
+    // 最大堆保存最大的k个数，堆顶是最大的，每次和新元素比较，若小于堆顶则替代堆顶
     vector<int> GetLeastNumbers_Solution(vector<int> input, int k) {
-        auto& v = input;
-        int n = v.size();
-        if (k > n || k <= 0 || n == 0)
+        int n = input.size();
+        if (k == 0 || n < k)
             return {};
-        std::priority_queue<int, vector<int>, std::greater<int>> q;
-        for (int i : input)
-            q.push(i);
-        vector<int> res;
-        res.reserve(k);
-        for (int i = 0; i < k; ++i) {
-            res.push_back(q.top());
+        priority_queue<int> q;  // 最大堆
+        for (int i = 0; i < k; i++)
+            q.push(input[i]);
+        for (int i = k; i < n; i++) {
+            if (q.top() > input[i]) {
+                q.pop();
+                q.push(input[i]);
+            }
+        }
+        vector<int> v;
+        while (!q.empty()) {
+            v.push_back(q.top());
             q.pop();
         }
-        return res;
+        return v;
     }
-	
-	// 用堆算法，根据k的大小选择构造最小堆还是最大堆
-/*    vector<int> GetLeastNumbers_Solution(vector<int> input, int k) {
-        auto& v = input;
-        int n = v.size();
-        if (k > n || k <= 0 || n == 0)
-            return {};
-        if (k > n / 2) {  // 最大堆，将最大的n-k个元素剔除
-            make_heap(v.begin(), v.end());
-            for (int i = 0; i < n - k; ++i)
-                pop_heap(v.begin(), v.end() - i);
-            v.resize(k);
-            return v;
-        } else {  // 最小堆，将最小的k个元素加入新的vector中
-            vector<int> res;
-            res.reserve(k);
-            auto pred = std::greater<int>();
-            make_heap(v.begin(), v.end(), pred);
-            for (int i = 0; i < k; i++) {
-                res.push_back(v[0]);
-                pop_heap(v.begin(), v.end() - i, pred);
-            }
-            return res;
-        }
-    }*/
 
     // 闭区间[low, high]，未检查low>high
 /*    int Partition(vector<int>& v, int low, int high) {
@@ -65,7 +50,7 @@ public:
         v[low] = pivot;
         return low;
     }
-    
+
     // 即找出第k+1大的数字，input[0..k-1]即最小的k个数字
     vector<int> GetLeastNumbers_Solution(vector<int> input, int k) {
         auto& v = input;
