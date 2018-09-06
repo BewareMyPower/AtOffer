8a9fc068af958bd12d69b1b7c47d608bdc610df2
BewareMyPower <xyzinfernity@163.com>
2018-09-06 18:49:42 +0800

    简化代码

--- 剑指offer/数组中只出现一次的数字.cc
+++ 剑指offer/数组中只出现一次的数字.cc
@@ -1,3 +1,6 @@
+#include <vector>
+using std::vector;
+
 /**
  * 一个整型数组里除了两个数字之外，其他的数字都出现了两次。请写程序找出这两个只出现一次的数字。
  */
@@ -7,31 +10,20 @@ public:
     // 2. 因此若data累计xor运算结果为z，则z=x*y且x!=y，其中*num1=x，*num2=y
     // x!=y意味着x和y必有一位分别为1和0，找到这一位后可以根据这一位是1还是0
     // 把整个数组分成2个序列，x和y分别分布在2个序列中，相同的元素肯定分布在同一个
-    // 子序列中，因此x和y为两个子序列的xor累计和
-    void FindNumsAppearOnce(vector<int> data,int* num1,int *num2) {
-        int xor_sum = accumulate(data.cbegin(), data.cend(), 0, bit_xor<int>());
-        int k = FindFirstBitsIs1(xor_sum);
-        *num1 = 0;
-        *num2 = 0;
+    // 子序列中，因此x和y为所在的子序列的异或累积和
+    void FindNumsAppearOnce(vector<int> data, int* num1,int *num2) {
+        int sum = 0;
+        for (int x : data)
+            sum ^= x;
+        int mask = 1;
+        while (!(sum & mask))
+            mask <<= 1;
+        *num1 = *num2 = 0;
         for (int x : data) {
-            if (BitsIs1(x, k))
+            if (x & mask)
                 *num1 ^= x;
             else
                 *num2 ^= x;
         }
-    }
-    
-    // 若x的二进制表示中从低位开始第idx(>=0)位为1则返回true
-    bool BitsIs1(int x, int idx) {
-        return (x & (1 << idx)) != 0;
-    }
-    
-    // 找到x的二进制表示中从低位开始为1的位的下标，下标从最低位为0
-    int FindFirstBitsIs1(int x) {
-        for (int i = 0; i < 8 * (int)sizeof(int); ++i) {
-            if ((x & (1 << i)) != 0)
-                return i;
-        }
-        return -1;  // 若x不为0则不可能到达这一步
     }
 };
